7cf631a205a54b15c3bc313033d0bd8a810a6a8d
Heluan <heluan.caetano@gmail.com>
2021-04-21 12:45:00 -0300

    Colors style dark

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7cecaef142eea69b77d3";
+/******/ 	var hotCurrentHash = "24e07d5ceda93a343875";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
